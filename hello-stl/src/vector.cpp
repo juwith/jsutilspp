817c9e52a15c73d677195f68a41018153e06b4e3
JS.Maeng <maeng@telechips.com>
2020-01-29 11:26:13 +0900

    vector : add comment
    Change-Id: Ife96c4c2e489386e5b16f73129d12abfa2f39667

--- hello-stl/src/vector.cpp
+++ hello-stl/src/vector.cpp
@@ -22,24 +22,25 @@ int main()
 	vect.push_back(3);
 	vect.push_back(4);
 
+	// vector is {1,2,3,4}
+	printVect(vect); 
+	vect.pop_back(); // pop : 4 , vector is {1,2,3}
 
 	printVect(vect);
-	vect.pop_back();
 
-	printVect(vect);
+	cout << "vect.at(1) : " << vect.at(1) << endl; // vector is {1,2,3}, at(0) 1, at(1) 2, ...
+	cout << "vect front : " << vect.front() << endl; // vector is {1,2,3}, front() is 1
+	cout << "vect back : " << vect.back() << endl; // vector is {1,2,3}, back() is 3
 
-	cout << "vect.at(1) : " << vect.at(1) << endl;
-	cout << "vect front : " << vect.front() << endl;
-	cout << "vect back : " << vect.back() << endl;
-
-	vect.insert(vect.begin(),50);
-	vect.erase(vect.end()-1);
+	vect.insert(vect.begin(),50); // insert 50, vector is {50, 1,2,3}
+	vect.erase(vect.end()-1); // earse last, vector is {50, 1,2}
 	printVect(vect);
 	
 
-	vector<int> vect2 = {11,12,13};
-	vect.swap(vect2);
+	vector<int> vect2 = {11,12,13}; // vect is {50,1,2}, vect2 is {11,12,13}
+	vect.swap(vect2); // vect is {11,12,13}, vect2 is {50,1,2}
 	printVect(vect);
+	printVect(vect2);
 
 	return 0;
 }
