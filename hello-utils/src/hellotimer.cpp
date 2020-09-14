8620cb0ffbc4da630c16da267ac173e55b5194fe
JS.Maeng <maeng@telechips.com>
2020-09-14 14:25:38 +0900

    hellotimer : orginize code
    Change-Id: Idde4293565a730bdda8faea9abb4cbc5ee780f64

--- hello-utils/src/hellotimer.cpp
+++ hello-utils/src/hellotimer.cpp
@@ -7,11 +7,6 @@
 
 #include <chrono>
 
-using namespace std;
-
-
-#define CLOCKS_PER_MS (CLOCKS_PER_SEC / 1000)
-
 int main()
 {
 	std::chrono::system_clock::time_point tic = std::chrono::system_clock::now();
@@ -22,16 +17,8 @@ int main()
 
 	std::chrono::duration<double> DefaultSec = toc-tic;
 
-	std::chrono::duration<double> test;
 
-	for(int i=0 ; i<7; i++) {
-		test += DefaultSec;
-	}
-
-	printf("%f s %f ms \n", DefaultSec.count()*1000 ,test.count()*1000);
-
-	int testid=1000;
-	printf("%d \n",testid%1000);
+	printf("%f ms \n", DefaultSec.count()*1000);
 
 	return 0;
 }
