b709d8c238b31ac278c5a538f197d295fa118c48
JS.Maeng <maeng@telechips.com>
2020-11-10 12:14:00 +0900

    parsing : update info
    Change-Id: Ibb85ce2e60f12cb9ece390e330404044559988fb

--- hello-strparsing/src/hellostrparsing.cpp
+++ hello-strparsing/src/hellostrparsing.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <map>
+
+std::map<std::string, int> getTunedValue(std::string type)
+{
+	std::map<std::string, int> tuned;
+
+	std::string line;
+	std::size_t found;
+
+	std::ostringstream setting_file;
+	setting_file << "mysetting";
+	std::ifstream settings(setting_file.str().c_str());
+	if( !settings.fail() && !settings.eof())
+	{
+		while( std::getline(settings, line) ) {
+			found = line.find("#");
+			if( found != std::string::npos ) {
+				line = line.substr(found + 1);
+				continue;
+			}
+			found = line.find(type.c_str());
+			if( found != std::string::npos ) {
+				std::size_t sepBegin = line.find(":");
+				std::size_t sepEnd = line.find("=");
+
+				std::string separator = line.substr(sepBegin+1, sepEnd-sepBegin-1);
+
+				sepBegin = sepEnd;
+				std::string targetVal = line.substr(sepBegin+1);
+				uint32_t finetuned = atoi(targetVal.c_str());
+
+				tuned[separator] = finetuned;
+				continue;
+			}
+
+		}
+	}
+	return tuned;
+}
 
 /**
  *   'mysetting' file required in your path.
@@ -14,40 +53,14 @@
 
 int main()
 {
-	std::string line;
-	std::size_t found;
+	std::map<std::string, int> tuned = getTunedValue("audio");
 
-	std::ostringstream setting_file;
-	setting_file << "./mysetting";
-	std::ifstream settings(setting_file.str().c_str());
-	if( !settings.fail() && !settings.eof())
-	{
-		while( std::getline(settings, line) ) {
-			found = line.find("#");
-			if( found != std::string::npos ) {
-				line = line.substr(found + 1);
-				continue;
-			}
-			found = line.find("my:age");
-			if( found != std::string::npos ) {
-				found = line.find("=");
-				line = line.substr(found + 1);
-				//atoi(line.c_str());
-				std::cout << "found: " << line.c_str() << std::endl;
-
-				continue;
-			}
-			found = line.find("my:name");
-			if( found != std::string::npos ) {
-				found = line.find("=");
-				line = line.substr(found + 1);
-				std::cout << "found: " << line.c_str() << std::endl;
-
-				continue;
-			}
+	std::cout << "audio information :" << std::endl;
+	for (std::map<std::string,int>::iterator it=tuned.begin(); it!=tuned.end(); ++it) {
+		if(it->first.compare("myval") == 0) {
+			std::cout << it->second << '\n';
 		}
+		std::cout << it->first << " => " << it->second << '\n';
 	}
-	
-	std::cout << line << std::endl;
 	return 0;
 }
