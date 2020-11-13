52c57d166bff8023d089534d79cb458d5bef4051
JS.Maeng <maeng@telechips.com>
2020-11-13 17:00:04 +0900

    heapsort : add minheap
    Change-Id: I3b0b5d28f55e20d6bf3f1854504d0fc971705bcb

--- sort-heap/src/heapsort.cpp
+++ sort-heap/src/heapsort.cpp
@@ -87,8 +87,95 @@ public:
 	}
 };
 
+class MinHeap
+{
+	vector<int> array;
+
+public:
+	MinHeap() {}
+	void insert(int x)
+	{
+		array.push_back(x);
+		int target = array.size();
+		while( target > 1 && x < array[(target/2)-1]) {
+			array[target-1] = array[(target/2)-1];
+			//cout << "target = " << target << ", target/2 = " << target/2 << endl;
+			target /= 2;
+			//cout << "target:" << target << endl;
+		}
+		//cout << "======end target========target:" << target << endl;
+		array[target-1] = x;
+	}
+
+	int popMin()
+	{
+		if(array.empty())
+			return -1;
+
+		//max is always first factor by heapify(insert)
+		int max = array.front();
+
+		//end factor moves to first factor
+		array.front() = array.back();
+		array.pop_back();
+
+		//heapify
+		int x = array.front();
+		int parent=1, child=2;
+		int arrSize = array.size();
+		while(child < arrSize) {
+			if(array[child-1] > array[child]) {
+				child++;
+			}
+			if(x <= array[child-1]) {
+				break;
+			}
+			array[parent-1] = array[child-1];
+			parent=child;
+			child *= 2;
+		}
+		array[parent-1] = x;
+
+		return max;
+	}
+
+	bool isEmpty()
+	{
+		if(array.empty())
+			return true;
+		return false;
+	}
+
+	void print()
+	{
+		cout << endl << "=============print=============" << endl;
+		for (vector<int>::const_iterator item=array.begin(); item != array.end();++item) {
+			cout<<*item << " ";
+		}
+		cout << endl << "===============================" << endl;
+	}
+
+	void printtree(int index, int space)
+	{
+		if(array.size() < index) {
+			return;
+		}
+
+		space += 4;
+		printtree((index*2)+1, space); //right
+		for(int i=4;i<space;i++) {
+			cout << " ";
+		}
+
+		cout << array[index-1] << endl;
+		printtree((index*2), space); //left
+	}
+};
+
+
 int main()
 {
+///////max sort
 	MaxHeap myheap;
 	myheap.insert(60);
 	myheap.insert(18);
@@ -108,16 +195,51 @@ int main()
 	while(!myheap.isEmpty()) {
 		int max = myheap.popMax();
 		sortmax.push_back(max);
-		
+		/*
 		cout << endl <<"pop max : " << max << endl;
 		cout << "--------------tree-------------" << endl;
 		myheap.printtree(1,0);
 		cout << endl << "-------------------------------" << endl;
-		
+		*/		
 	}
 
 	cout << "max sort : ";
 	for (vector<int>::const_iterator i=sortmax.begin(); i != sortmax.end();++i) {
+		cout<<*i << " ";
+	}
+	cout << endl;
+
+
+///////////////min sort
+
+	MinHeap minheap;
+	minheap.insert(10);
+	minheap.insert(5);
+	minheap.insert(20);
+	minheap.insert(3);
+	minheap.insert(4);
+	minheap.insert(100);
+	minheap.insert(120);
+	minheap.insert(130);
+	cout << "--------------tree-------------" << endl;
+	minheap.printtree(1,0);
+	cout << "-------------------------------" << endl;
+
+	vector<int> sortmin;
+	while(!minheap.isEmpty()) {
+		int min = minheap.popMin();
+		sortmin.push_back(min);
+		/*
+		cout << endl <<"pop min : " << min << endl;
+		cout << "--------------tree-------------" << endl;
+		minheap.printtree(1,0);
+		cout << endl << "-------------------------------" << endl;
+		*/
+		
+	}
+
+	cout << "min sort : ";
+	for (vector<int>::const_iterator i=sortmin.begin(); i != sortmin.end();++i) {
 		cout<<*i << " ";
 	}
 	cout << endl;
